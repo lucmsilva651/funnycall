fdfa198a54f67abeda6231fafbe12a2a424f4076
Lucas Gabriel <lucmsilva651@gmail.com>
2024-10-06 08:59:05 -0300

    Removed cringy 2022 meme + some other changes

--- src/main.py
+++ src/main.py
@@ -4,12 +4,8 @@ from PIL import Image, ImageTk
 import pygame
 import os
 
-os.system('cls')
 print("funnyCall - Made by Lucas Gabriel (lucmsilva)")
-print("https://github.com/lucmsilva651/funnycall")
-os.system('echo.')
-print("Using Pillow, Tkinter and pygame")
-os.system('echo.')
+print("https://github.com/lucmsilva651/funnycall\n")
 
 def call_boykisser():
     pygame.mixer.music.load('files/mp3/1928491298.mp3')
@@ -33,31 +29,6 @@ def call_boykisser():
         label.image = frame_image
         dialog_window.update()
         dialog_window.after(100)
-
-
-def call_wheredidyougo():
-    pygame.mixer.music.load('files/mp3/1706270076.mp3')
-    pygame.mixer.music.play()
-    messagebox.showinfo("Call: young fly on the track", "young fly on the track (niggers, niggers, niggers)")
-    print("young fly on the track (niggers, niggers, niggers)")
-    dialog_window = tk.Toplevel(root)
-    dialog_window.title("young fly on the track")
-    dialog_window.iconbitmap('files/icon/blank.ico')
-    gif_path = 'files/gif/1706270076.gif'
-    gif = Image.open(gif_path)
-    gif_width, gif_height = gif.size
-    label = tk.Label(dialog_window, image=None)
-    label.pack()
-    frames = gif.n_frames
-    for i in range(frames):
-        gif.seek(i)
-        frame = gif.copy().resize((gif_width, gif_height))
-        frame_image = ImageTk.PhotoImage(frame)
-        label.configure(image=frame_image)
-        label.image = frame_image
-        dialog_window.update()
-        dialog_window.after(10)
-
 
 def call_fspkchinese():
     pygame.mixer.music.load('files/mp3/8261562652.mp3')
@@ -89,21 +60,16 @@ def button_click(number):
     entry.insert(tk.END, new_number)
     if new_number == '1928491298':
         call_boykisser()
-    if new_number == '1706270076':
-        call_wheredidyougo()
     if new_number == '8261562652':
         call_fspkchinese()
         
-
 def clear_number():
     entry.delete(0, tk.END)
     
-
 def create_button(frame, number):
     button = tk.Button(frame, text=str(number), width=5, height=2,
                        command=lambda: button_click(number))
     button.pack(side=tk.LEFT)
-
 
 pygame.mixer.init()
 root = tk.Tk()
