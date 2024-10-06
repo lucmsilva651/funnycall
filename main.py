0fb42ada785965f3178c528ce57b7fbd422bb800
Lucas Gabriel <lucmsilva651@gmail.com>
2024-10-06 09:17:05 -0300

    Moved everything outside src + some code fixes and additions

--- main.py
+++ main.py
@@ -2,20 +2,24 @@ import tkinter as tk
 from tkinter import messagebox
 from PIL import Image, ImageTk
 import pygame
+from pyfiglet import Figlet
 import os
+
+# print to terminal (idk why but ill do a figlet here)
+f = Figlet(font='standard')
+print(f.renderText('lucmsilva'))
 
 print("funnyCall - Made by Lucas Gabriel (lucmsilva)")
 print("https://github.com/lucmsilva651/funnycall\n")
 
 def call_boykisser():
-    pygame.mixer.music.load('files/mp3/1928491298.mp3')
+    pygame.mixer.music.load('assets/mp3/1928491298.mp3')
     pygame.mixer.music.play()
     messagebox.showinfo("Call: boykisser", "boykisser!")
     print("boykisser!")
     dialog_window = tk.Toplevel(root)
     dialog_window.title("boykisser")
-    dialog_window.iconbitmap('files/icon/blank.ico')
-    gif_path = 'files/gif/1928491298.gif'
+    gif_path = 'assets/gif/1928491298.gif'
     gif = Image.open(gif_path)
     gif_width, gif_height = gif.size
     label = tk.Label(dialog_window, image=None)
@@ -31,14 +35,13 @@ def call_boykisser():
         dialog_window.after(100)
 
 def call_fspkchinese():
-    pygame.mixer.music.load('files/mp3/8261562652.mp3')
+    pygame.mixer.music.load('assets/mp3/8261562652.mp3')
     pygame.mixer.music.play()
     messagebox.showinfo("Call: frog speaking chinese", "你以下業務出現違規異常，了解詳情請按0， 由華語客服為你服務")
     print("你以下業務出現違規異常，了解詳情請按0， 由華語客服為你服務")
     dialog_window = tk.Toplevel(root)
     dialog_window.title("frog speaking chinese")
-    dialog_window.iconbitmap('files/icon/blank.ico')
-    gif_path = 'files/gif/8261562652.gif'
+    gif_path = 'assets/gif/8261562652.gif'
     gif = Image.open(gif_path)
     gif_width, gif_height = gif.size
     label = tk.Label(dialog_window, image=None)
@@ -74,7 +77,6 @@ def create_button(frame, number):
 pygame.mixer.init()
 root = tk.Tk()
 root.title("funnyCall")
-root.iconbitmap('files/icon/blank.ico')
 entry = tk.Entry(root, width=45)
 entry.pack()
 button_frame = tk.Frame(root)
